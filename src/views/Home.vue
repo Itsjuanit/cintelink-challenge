5748970c58e509795be953cf7e075093f44ca1a7
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2022-05-07 01:34:51 -0300

    Se cambio el front de la vista del Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,91 @@
 <template>
-  <div class="home">Home</div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="pt-4"
+      color="grey lighten-3"
+      mini-variant
+    >
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      ></v-avatar>
+    </v-navigation-drawer>
+
+    <v-main>
+      <h1>Bienvenidos al Home de CintelinkApp</h1>
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+          height="250"
+          src="https://images.unsplash.com/photo-1612725718815-64705bf42363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80"
+        ></v-img>
+
+        <v-card-title>Cintelink</v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">• Cordoba</div>
+
+          <div>
+            Small plates, salads & sandwiches - an intimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-text>
+          <v-chip-group
+            v-model="selection"
+            active-class="deep-purple accent-4 white--text"
+            column
+          >
+            <v-chip>5:30PM</v-chip>
+
+            <v-chip>7:30PM</v-chip>
+
+            <v-chip>8:00PM</v-chip>
+
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="deep-purple lighten-2" text @click="reserve">
+            ACTIVA
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  //
+};
 </script>
